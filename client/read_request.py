e5b0ab4df864d0d13224689f10b4db49a777d386
EvaOrange73 <evaorange73@gmail.com>
2022-03-02 15:19:03 +0300

    read request

---
+++ client/read_request.py
@@ -0,0 +1,39 @@
+import sys
+import json
+
+
+async def get_request():
+    print("Hello!\n"
+          "I will return the first 10 search results on aliexpress.com\n"
+          "I can process requests in json format, for example:\n"
+          "{\"searchWord\": \"iphone\"}")
+
+    request = ""
+    waiting_for_the_next_line = False
+
+    for line in sys.stdin:
+        if line.startswith("{"):
+            waiting_for_the_next_line = True
+
+        if waiting_for_the_next_line:
+            request += line
+        else:
+            print("maybe you forgot \"{\"")
+
+        if line.endswith("}\n"):
+            waiting_for_the_next_line = False
+            try:
+                request = json.loads(request)
+            except ValueError as e:
+                print("I need json format")
+                request = ""
+                continue
+
+            if request.get("searchWord") is None:
+                print("i need searchWord")
+                request = ""
+                continue
+
+            # TODO: send request
+
+            request = ""
---
+++ main.py
@@ -0,0 +1,5 @@
+from client.read_request import read_request
+
+
+def main():
+    read_request()
