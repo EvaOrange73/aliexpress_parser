22504735ba30fb38403e09bba4be119aa98c9040
EvaOrange73 <evaorange73@gmail.com>
2022-03-06 14:34:13 +0300

    search error, connection error

--- server/parser.py
+++ server/parser.py
@@ -5,12 +5,21 @@ import requests
 
 
 def parser(search_word):
-    answer = {"search": search_word, "result": []}
+    answer = {"search": search_word}
     link = "https://aliexpress.ru/wholesale?catId=&SearchText=" + search_word
     soup = BeautifulSoup(requests.get(link).text, 'html.parser')
 
-    results = soup.find_all("div", {"class": "product-snippet_ProductSnippet__content__mdters"}, limit=10)
+    search_error = soup.find_all("div", {"class": "SearchWrap_SearchError__error__oy8dw"})
+    if search_error:
+        answer['error'] = "search error"
+        return json.dumps(answer, ensure_ascii=False)
 
+    results = soup.find_all("div", {"class": "product-snippet_ProductSnippet__content__mdters"}, limit=10)
+    if not results:
+        answer['error'] = "connection error"
+        return json.dumps(answer, ensure_ascii=False)
+
+    answer['result'] = []
     for result in results:
 
         has_discount = result.find_all("div", {"class": "snow-price_SnowPrice__discountPercent__18s9w6"})
